5fccf9bbac3b59bc1589cdb2c64cd2efbbea5286
Mona Lisa <gugacns94@gmail.com>
2019-07-16 20:43:31 +0200

    vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+  state : {
+    counter: 0
+  }
+});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
