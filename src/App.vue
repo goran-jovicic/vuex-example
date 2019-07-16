f9655909d1774f8c41e8395b441bcd4bc1916371
Mona Lisa <gugacns94@gmail.com>
2019-07-16 21:22:28 +0200

    gogogog

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,28 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <h2>{{ counter }}</h2>
+    <button @click="incrementState">Increment</button>
   </div>
 </template>
+
+<script>
+  export default {
+    computed : {
+      counter() {
+        // return this.$store.state.counter;
+        return this.$store.getters.getCountText
+      }
+    },
+
+    methods : {
+      incrementState() {
+        // this.$store.commit('increment')
+        this.$store.dispatch('incrementAfterTwoSeconds')
+      }
+    }
+  }
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -3,15 +3,35 @@ import App from './App.vue'
 import router from './router'
 import Vuex from 'vuex'
 
+Vue.use(Vuex)
+
 const store = new Vuex.Store({
   state : {
-    counter: 0
+    counter: 100
+  },
+  mutations : {
+    increment(state){
+      state.counter++
+    }
+  },
+  actions: {
+    incrementAfterTwoSeconds(context) {
+      setTimeout(() => {
+        context.commit('increment')
+      }, 2000)
+    }
+  },
+  getters : {
+    getCountText(state) {
+      return `Hello, ${state.counter}`
+    }
   }
 });
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
