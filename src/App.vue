f0f86637103c491be26a20cec81b947b19e69012
Mona Lisa <gugacns94@gmail.com>
2019-07-18 18:39:37 +0200

    mapGetters begininning

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
     <h2>{{ counter }}</h2>
     <button @click="incrementState">Increment</button>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
   export default {
     computed : {
-      counter() {
-        // return this.$store.state.counter;
-        return this.$store.getters.getCountText
-      }
+      ...mapGetters({
+        counter : 'getCountText'
+      }),
+      // counter() {
+      //   // return this.$store.state.counter;      // novi nacin
+      //   return this.$store.getters.getCountText
+      // }
     },
 
     methods : {
@@ -30,7 +35,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
